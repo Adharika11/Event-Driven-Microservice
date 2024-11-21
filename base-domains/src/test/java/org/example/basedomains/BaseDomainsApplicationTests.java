ef5a84c2faeb5b557cce778f0d387d59b1281372
Adharika11 <adhs.gs2003@gmail.com>
2024-11-21 12:36:17 +0530

    Initial commit

---
+++ base-domains/src/test/java/org/example/basedomains/BaseDomainsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.basedomains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseDomainsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
